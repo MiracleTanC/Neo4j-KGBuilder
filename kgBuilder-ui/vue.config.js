e4499a1b96944bd0dbda2c3cbadfe9fa65eb47aa
暖暖动听 <1130196938@qq.com>
2022-03-17 16:09:07 +0800

    Update vue.config.js

--- kgBuilder-ui/vue.config.js
+++ kgBuilder-ui/vue.config.js
@@ -35,8 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8099/kg`,
-        //target: `http://www.miaoleyan.com:8080`,
+        target: `http://localhost:8080`,
         changeOrigin: true,
         pathRewrite: {
           ["^" + process.env.VUE_APP_BASE_API]: ""
